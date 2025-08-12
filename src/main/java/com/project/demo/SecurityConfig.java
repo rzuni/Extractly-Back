8b2fe0b1e443533bf51768a71b011942fbf4c72d
Stephabarca <stephabarca29@gmail.com>
2025-08-12 13:49:41 -0600

    Authentication with Google

---
+++ src/main/java/com/project/demo/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.project.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/loginSuccess")
+                        .failureUrl("/loginFailure")
+                );
+        return http.build();
+    }
+}
