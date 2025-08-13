18f1d499840c82d8b97a397918358ed8ab6ed112
Stephabarca <stephabarca29@gmail.com>
2025-08-12 22:32:23 -0600

    google sign in impl

--- src/main/java/com/project/demo/AuthController.java
+++ src/main/java/com/project/demo/AuthController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+
+
 @RestController
 public class AuthController {
 
--- src/main/java/com/project/demo/CorsConfig.java
+++ src/main/java/com/project/demo/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:4200/");
+        config.addAllowedOrigin("http://localhost:4200"); // Elimina la barra final
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
